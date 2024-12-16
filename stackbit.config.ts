8387c23ba435ce3761338c0540e78e0f1c057bd4
Stango19 <stango1210@gmail.com>
2024-12-16 18:35:02 +0000

    Create stackbit.config.ts

---
+++ stackbit.config.ts
@@ -0,0 +1,6 @@
+export default {
+  projectId: "your-project-id", // Replace with your Stackbit project ID
+  devCommand: "npm run dev",     // Replace with the correct command for your project
+  buildCommand: "npm run build", // Replace with your build command, if necessary
+  siteName: "your-site-name",    // Replace with your site name
+};
